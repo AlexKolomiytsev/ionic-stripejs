4899c0dd4a73b72ccfe9bc5590cd1268273ec758
alexanderkolomiytsev <alexander.kolomiytsev@gmail.com>
2022-07-18 17:46:53 +0300

    fix errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import { Redirect, Route } from 'react-router-dom';
 import {
   IonApp,
-  IonIcon,
-  IonLabel,
   IonRouterOutlet,
-  IonTabBar,
-  IonTabButton,
-  IonTabs,
   setupIonicReact
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
 
 import {Elements} from '@stripe/react-stripe-js';
 import {loadStripe} from '@stripe/stripe-js';
--- src/pages/StripePage.tsx
+++ src/pages/StripePage.tsx
@@ -1,5 +1,5 @@
-import {IonButton, IonContent, IonHeader, IonLoading, IonPage, IonText} from '@ionic/react';
-import {useState, useEffect} from 'react';
+import {IonButton, IonContent, IonPage, IonText, IonSpinner} from '@ionic/react';
+import React, {useState} from 'react';
 import {useStripe, useElements, CardElement} from '@stripe/react-stripe-js';
 
 const StripePage: React.FC = () => {
@@ -7,8 +7,8 @@ const StripePage: React.FC = () => {
   const stripe = useStripe();
   const elements = useElements();
 
-  const [response, setResponse] = useState<string>();
-  const [token, setToken] = useState<string>();
+  const [intent, setIntent] = useState<string>();
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (event: any) => {
     event.preventDefault();
@@ -19,11 +19,12 @@ const StripePage: React.FC = () => {
 
     try {
       if (elements && elements.getElement(CardElement)) {
+        setLoading(true);
 
         const response = await fetch('https://capable-parfait-de86c8.netlify.app/.netlify/functions/create-payment-intent');
-        const { clientSecret } = await response.json();
+        const {clientSecret} = await response.json();
 
-        const { paymentIntent } = await stripe
+        await stripe
           .confirmCardPayment(clientSecret, {
             payment_method: {
               // @ts-ignore
@@ -36,6 +37,9 @@ const StripePage: React.FC = () => {
 
         const intent = await stripe.retrievePaymentIntent(clientSecret);
 
+        console.log('intent', intent);
+
+        setIntent(JSON.stringify(intent.paymentIntent, null, 2));
 
         // stripe.createPaymentMethod({
         //   type: 'card',
@@ -56,8 +60,10 @@ const StripePage: React.FC = () => {
         //   });
       }
     } catch (e) {
-      console.log(e);
-      setResponse(String(e));
+      console.log('error', e);
+      setIntent(String(e));
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -65,13 +71,21 @@ const StripePage: React.FC = () => {
     <IonPage>
       <IonContent style={{height: '100vh', width: '100vw'}}>
         <div style={{position: 'relative', top: '30%', width: '90%', margin: 'auto'}}>
-          <form onSubmit={handleSubmit}>
-            <CardElement options={{hidePostalCode: true}}/>
-            <IonButton disabled={!stripe} type="submit">Submit</IonButton>
-          </form>
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end', justifyContent: 'flex-end' }}>
+            <div style={{ border: '1px solid black', padding: '20px', width: '100%', marginBottom: '20px' }}>
+              <CardElement options={{hidePostalCode: true}}/>
+            </div>
+            <IonButton
+              disabled={!stripe || loading}
+              style={{ minWidth: '120px' }}
+              type="submit"
+              onClick={handleSubmit}
+            >
+              {loading ? <IonSpinner name="dots" /> : <span>Submit</span>}
+            </IonButton>
+          </div>
           <div style={{paddingTop: '20px', display: 'flex', flexDirection: 'column'}}>
-            <IonText>{response}</IonText>
-            <IonText>{token}</IonText>
+            <IonText>{intent}</IonText>
           </div>
         </div>
       </IonContent>
