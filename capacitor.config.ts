2ae3587fea067bc6306c66842df0c745248ec8bd
alexanderkolomiytsev <alexander.kolomiytsev@gmail.com>
2022-07-18 17:52:58 +0300

    fix errors

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'stripe-testing',
+  appId: '404f32d3',
+  appName: 'test-ionic-with-stripe',
   webDir: 'build',
   bundledWebRuntime: false,
   server: {
