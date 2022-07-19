00050630e20da7fa2b779e52cafb13c3262926af
alexanderkolomiytsev <alexander.kolomiytsev@gmail.com>
2022-07-19 13:29:19 +0300

    FISIKAL: rm const from gitignore

---
+++ src/utils/const.ts
@@ -0,0 +1,11 @@
+export var auth = {
+  // url: 'https://egym.fisikal.com/',
+  url: 'https://staging.fisikal.com',
+  fisikal_token: 'eyJpZCI6NDg2MCwicmVkaXJlY3RfdXJsIjpudWxsLCJzaWciOiI3NGI3OGYwOWY3OGEzZjZkNWU3MGFmNmEwMGUyYTQyMWEwMWY4NGNjNGM5NTFiNDM1ZjcxY2FhMDFmZjU2OTkzIn0=',
+  club: {
+    id: 0,
+    name: '',
+    image_url: '',
+    time_zone: ''
+  }
+};
